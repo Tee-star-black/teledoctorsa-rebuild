f4451dd7ea2f43624e274871e85e87b577cad14f
Tee-star-black <tntshabeleng23@gmail.com>
2026-08-15 18:20:08 +0200

    feat: assemble reworked homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import "./home.css";
+
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
 import { Hero } from "@/components/home/hero";
+import { HomeSections } from "@/components/home/home-sections";
 
 export default function HomePage() {
   return (
@@ -9,6 +12,7 @@ export default function HomePage() {
 
       <main>
         <Hero />
+        <HomeSections />
       </main>
 
       <Footer />
