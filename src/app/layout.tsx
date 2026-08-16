d07767dcc148358db9d35adb469b4d9f5f11e8e2
Tee-star-black <tntshabeleng23@gmail.com>
2026-08-16 19:46:14 +0200

    style: load compact site scale globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./centered-marketing.css";
+import "./minimal-scale.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
